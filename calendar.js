a1e131a8fb26b8c47cb59f536006e5b345a94753
chmanie <chmanie@gmail.com>
2014-04-08 16:44:36 +0200

    add drag&drop

--- calendar.js
+++ calendar.js
@@ -12,11 +12,32 @@ body {
 
   angular.module('scrollingCalendar', []);
 
+  angular.module('scrollingCalendar').factory('calListeners', function(){
+    var listeners = {};
+    var scope;
+    return {
+      setScope: function (scp) {
+        scope = scp;
+      },
+      onDrop: function (cb) {
+        listeners.drop = cb;
+      },
+      drop: function (calItem, targetDay, originDay) {
+        if (listeners.drop) {
+          listeners.drop(scope, {
+            $item: calItem,
+            $targetDay: targetDay,
+            $originDay: originDay
+          });
+        }
+      }
+    };
+  });
+
   var calName = 'calendar';
 
-  angular.module('scrollingCalendar').directive(calName, function($window, $document, $timeout){
+  angular.module('scrollingCalendar').directive(calName, function($window, $document, $timeout, $compile, calListeners, $parse){
     // Runs during compile
-
 
     return {
       // name: '',
@@ -37,17 +58,17 @@ body {
         var originalDocument = $document[0];
         var parentElement = element.parent();
         var originalParentElement = parentElement[0];
-        var todayDate;
-        var todayElement;
-        var firstDate;
-        var lastDate;
+        var todayDate, todayElement, firstDate, lastDate, backgroundColor;
         var scrollDates = [];
-        var color = [233, 229, 236].join(',');
+        var defaultBackgroundColor = [233, 229, 236];
         var offset = 0.5;
         var speed = 2;
         var firstDayOfWeek = 1;
         var lastDaysOfWeek = [6, 0, 1, 2, 3, 4, 5];
         var currentMonth, nextMonth;
+
+        calListeners.setScope($scope);
+        calListeners.onDrop($parse(attrs.calDrop));
 
         $scope.currentScrollIndex = 0;
         $scope.$watch('currentScrollIndex', function (newIndex) {
@@ -83,24 +104,24 @@ body {
             
             if (percentage>offset && (percentage-offset)*speed/(1-offset) <= 1) {
               currentMonth.css({
-                'background-color': 'rgba(' + color + ', ' + ((percentage-offset)*speed/(1-offset)) + ')'
+                'background-color': 'rgba(' + backgroundColor + ', ' + ((percentage-offset)*speed/(1-offset)) + ')'
               });
               nextMonth.css({
-                'background-color': 'rgba(' + color + ', ' + (1-(percentage-offset)*speed/(1-offset)) + ')'
+                'background-color': 'rgba(' + backgroundColor + ', ' + (1-(percentage-offset)*speed/(1-offset)) + ')'
               });
             }
             if (percentage<offset) {
               currentMonth.css({
-                'background-color': 'rgba(' + color + ', 0)'
+                'background-color': 'rgba(' + backgroundColor + ', 0)'
               });
             }
 
             if ((percentage-offset)*speed/(1-offset) > 1) {
               currentMonth.css({
-                'background-color': 'rgba(218, 216, 222, ' + 1 + ')'
+                'background-color': 'rgba(' + backgroundColor + ', ' + 1 + ')'
               });
               nextMonth.css({
-                'background-color': 'rgba(218, 216, 222, ' + 0 + ')'
+                'background-color': 'rgba(' + backgroundColor + ', ' + 0 + ')'
               });
             }
           }
@@ -126,14 +147,27 @@ body {
         }
 
         function generateDay(day, date) {
+          var scope = $scope.$new();
+          scope.events = [{ title: 'Bla' }];
+          day = angular.element(day);
           var isToday = (date.getDate() === todayDate.getDate() && date.getMonth() === todayDate.getMonth() && date.getFullYear() === todayDate.getFullYear());
-          if (isToday) day.className += ' today';
+          if (isToday) day.addClass('today');
           if (!todayElement) todayElement = angular.element(day);
 
-          day.innerHTML = '<span>' + date.getDate() + ' ' + (date.getMonth()+1) + '</span>';
-          angular.element(day).addClass([date.getYear(), date.getMonth()].join('_'));
+          day.html(
+            '<span>' + date.getDate() + ' ' + (date.getMonth()+1) + '</span>' +
+            '<div style="height: 100px" cal-day="event in events">' +
+              '<div style="background-color:red;text-align:left" cal-entry cal-entry-draggable="\'true\'">' +
+                '<span class="event">{{event.title}}</span>' +
+              '</div>' +
+            '</div>'
+            );
+          day.addClass([date.getYear(), date.getMonth()].join('_'));
+
+          $compile(day)(scope);
+          
           angular.element(day).css({
-            'background-color': 'rgba(' + color + ', 1)'
+            'background-color': 'rgba(' + backgroundColor + ', 1)'
           });
 
         }
@@ -214,14 +248,32 @@ body {
 
           scrollDates.push({ month: seedDate.getMonth(), pos: week.offsetTop, year: lastDate.getYear() });
 
+          // prepend the first month
           prependMonth();
 
+          // append enough weeks
           for(var i = 1; i<=10; i++) appendWeek();
 
+          // scroll to today
           originalParentElement.scrollTop = todayElement[0].offsetTop;
+
+          backgroundColor = getBackgroundColor();
 
           // let the watcher trigger before start colorizing
           $timeout(colorizeMonth, 50);
+        }
+
+        function getBackgroundColor() {
+          var cssBackground;
+          var todayElm = originalDocument.getElementsByClassName('today')[0];
+          if (todayElm.currentStyle) {
+            cssBackground = todayElm.currentStyle.backgroundColor;
+          } else {
+            cssBackground = $window.getComputedStyle(todayElm)['backgroundColor'];
+          }
+          var color = cssBackground.match(/rgb\((\d{1,3}),\s(\d{1,3}),\s(\d{1,3})\)/).slice(1,4).join(',');
+          if (!color) return defaultBackgroundColor.join(',');
+          return color;
         }
 
         todayDate = new Date();
@@ -230,6 +282,310 @@ body {
 
       }
     };
+  });
+
+  /*
+   * forked from angular-dragon-drop v0.3.1
+   * (c) 2013 Brian Ford http://briantford.com
+   * License: MIT
+   */
+
+  angular.module('scrollingCalendar').directive('calDay', function ($document, $compile, $rootScope, calListeners, $timeout, $window) {
+
+    var body = $document[0].body,
+      dragValue,
+      dragKey,
+      dragOrigin,
+      floaty,
+      offsetX,
+      offsetY,
+      originElement,
+      originElemOffsetX,
+      originElemOffsetY;
+
+    var drag = function (ev) {
+      var x = ev.clientX - offsetX,
+          y = ev.clientY - offsetY;
+
+      floaty.css('left', x + 'px');
+      floaty.css('top', y + 'px');
+    };
+
+    var remove = function (collection, index) {
+      if (collection instanceof Array) {
+        return collection.splice(index, 1);
+      } else {
+        var temp = collection[index];
+        delete collection[index];
+        return temp;
+      }
+    };
+
+    var add = function (collection, item, key) {
+      if (collection instanceof Array) {
+        collection.push(item);
+      } else {
+        collection[key] = item;
+      }
+    };
+
+    var documentBody = angular.element($document[0].body);
+
+    var disableSelect = function () {
+      documentBody.css({
+        '-moz-user-select': '-moz-none',
+        '-khtml-user-select': 'none',
+        '-webkit-user-select': 'none',
+        '-ms-user-select': 'none',
+        'user-select': 'none'
+      });
+    };
+
+    var enableSelect = function () {
+      documentBody.css({
+        '-moz-user-select': '',
+        '-khtml-user-select': '',
+        '-webkit-user-select': '',
+        '-ms-user-select': '',
+        'user-select': ''
+      });
+    };
+
+    var killFloaty = function () {
+      if (floaty) {
+        $document.unbind('mousemove', drag);
+        floaty.remove();
+        floaty = null;
+        dragValue = dragOrigin = originElement = null;
+      }
+    };
+
+    var getElementOffset = function (elt) {
+
+      var box = elt.getBoundingClientRect();
+
+      var xPosition = box.left + body.scrollLeft;
+      var yPosition = box.top + body.scrollTop;
+
+      return {
+        left: xPosition,
+        top: yPosition
+      };
+    };
+
+    // Get the element at position (`x`, `y`) behind the given element
+    var getElementBehindPoint = function (behind, x, y) {
+      var originalDisplay = behind.css('display');
+      behind.css('display', 'none');
+
+      var element = angular.element($document[0].elementFromPoint(x, y));
+
+      behind.css('display', originalDisplay);
+
+      return element;
+    };
+
+    $document.bind('mouseup', function (ev) {
+      if (!dragValue) {
+        return;
+      }
+
+      enableSelect();
+      $document.unbind('mousemove', drag);
+
+      var dropArea = getElementBehindPoint(floaty, ev.clientX, ev.clientY);
+
+      var accepts = function () {
+        return !!dropArea.attr('cal-day');
+      };
+
+      while (dropArea.length > 0 && !accepts()) {
+        dropArea = dropArea.parent();
+      }
+
+      if (dropArea.length > 0) {
+
+        var expression = dropArea.attr('cal-day');
+        var targetScope = dropArea.scope();
+        var originScope = originElement.scope();
+        var match = expression.match(/^\s*(.+)\s+in\s+(.*?)\s*$/);
+
+        var targetList = targetScope.$eval(match[2]);
+
+        if (targetList !== dragOrigin) {
+          targetScope.$apply(function () {
+            add(targetList, dragValue, dragKey);
+          });
+
+          calListeners.drop(dragValue, targetScope.day, originScope.day);
+
+          $rootScope.$apply(function () {
+            remove(dragOrigin, dragKey || dragOrigin.indexOf(dragValue));
+          });
+          killFloaty();
+        } else {
+          originElement.css({ 'opacity': '1'});
+          killFloaty();
+        }
+      } else {
+        $timeout(function () {
+          floaty.css({
+            'transition': 'all 0.5s',
+            '-webkit-transition': 'all 0.5s',
+            'left': (originElemOffsetX - body.scrollLeft) + 'px',
+            'top': (originElemOffsetY - body.scrollTop) + 'px'
+          });
+          floaty.bind('webkitTransitionEnd', function () { // TODO: add other browser events
+            originElement.css({ 'opacity': '1'});
+            killFloaty();
+          });
+        }, 0);
+      }
+    });
+
+    return {
+      restrict: 'A',
+
+      compile: function (container, attr) {
+
+        // get the `thing in things` expression
+        var expression = attr.calDay;
+        var match = expression.match(/^\s*(.+)\s+in\s+(.*?)\s*$/);
+        if (!match) {
+          throw Error("Expected calDay in form of '_item_ in _collection_' but got '" +
+            expression + "'.");
+        }
+        var lhs = match[1];
+        var rhs = match[2];
+
+        match = lhs.match(/^(?:([\$\w]+)|\(([\$\w]+)\s*,\s*([\$\w]+)\))$/);
+
+        var valueIdentifier = match[3] || match[1];
+        var keyIdentifier = match[2];
+
+        // pull out the template to re-use.
+        // Improvised ng-transclude.
+        var template = container.html();
+        console.log(template);
+
+        // wrap text nodes
+        try {
+          template = angular.element(template.trim());
+          if (template.length === 0) {
+            throw new Error('');
+          }
+        }
+        catch (e) {
+          template = angular.element('<div>' + template + '</div>'); // TODO: better template
+        }
+        var child = template.clone();
+        child.attr('ng-repeat', expression);
+
+        container.html('');
+        container.append(child);
+
+        return function (scope, elt, attr) {
+
+          var spawnFloaty = function () {
+            scope.$apply(function () {
+              floaty = template.clone();
+              // floaty.bind('scroll', function (evt) {
+              //   console.log(evt);
+              // });
+              floaty.css({
+                'position': 'fixed',
+                'z-index': '99999',
+                'pointer-events': 'none'
+              });
+
+              // IE
+              if (!supporsPointerEvents()) floaty.css('margin-top', '20px');
+
+              $document.bind('scroll', function (evt) {
+                console.log(evt);
+              });
+
+              var floatyScope = scope.$new();
+              floatyScope[valueIdentifier] = dragValue;
+              if (keyIdentifier) {
+                floatyScope[keyIdentifier] = dragKey;
+              }
+              $compile(floaty)(floatyScope);
+              documentBody.append(floaty);
+              $document.bind('mousemove', drag);
+              disableSelect();
+            });
+          };
+
+          elt.bind('mousedown', function (ev) {
+            
+            originElement = angular.element(ev.target);
+
+            var originScope = originElement.scope();
+
+            var canDrag = originScope.$eval(child.attr('cal-entry-draggable'));
+
+            if (dragValue || !canDrag) {
+              return;
+            }
+
+            // find the right parent
+            while (originElement.attr('cal-entry') === undefined) {
+              originElement = originElement.parent();
+            }
+
+            while (originScope[valueIdentifier] === undefined) {
+              originScope = originScope.$parent;
+              if (!originScope) {
+                return;
+              }
+            }
+
+            dragValue = originScope[valueIdentifier];
+            dragKey = originScope[keyIdentifier];
+            if (!dragValue) {
+              return;
+            }
+
+            // get offset inside element to drag
+            var offset = getElementOffset(originElement[0]);
+
+            dragOrigin = scope.$eval(rhs);
+            // dragValue = angular.copy(dragValue);
+
+            offsetX = (ev.pageX - offset.left);
+            offsetY = (ev.pageY - offset.top);
+
+            originElemOffsetX = offset.left;
+            originElemOffsetY = offset.top;
+
+            spawnFloaty();
+            originElement.css({ 'opacity': '0'});
+            drag(ev);
+          });
+        };
+      }
+    };
+
+    function supporsPointerEvents() {
+      // from modernizr
+
+      var element = $document[0].createElement('x'),
+        documentElement = $document[0].documentElement,
+        getComputedStyle = $window.getComputedStyle,
+        supports;
+      if(!('pointerEvents' in element.style)){
+        return false;
+      }
+      element.style.pointerEvents = 'auto';
+      element.style.pointerEvents = 'x';
+      documentElement.appendChild(element);
+      supports = getComputedStyle &&
+        getComputedStyle(element, '').pointerEvents === 'auto';
+      documentElement.removeChild(element);
+      return !!supports;
+    }
+
   });
 
 
